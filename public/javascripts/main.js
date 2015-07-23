e1b1b4547e9fa876c7615e21ff0d70632e4c26e7
vikash2696 <vikash.kumar@osscube.com>
2015-07-23 15:42:54 +0530

    playing 11 team added

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,3 +1,7 @@
+/**
+ * @author vikash
+ */
+
 function deleteUser(id) {
 	var del = confirm("Are you confirm?");
 	if (del == true) {
@@ -19,23 +23,11 @@ $(document).on("click", '#list_view', function() {
 	});
 });
 
-/*function load_home(){ 
- document.getElementById("content").innerHTML='<object type="text/html" data="home.html" ></object>';
- }*/
-
-$(document).on("click", '.abc', function() {	
-	var userId = $(this).attr('id');
-	$.get('/edituser/' + userId  ,function( data ) {
-		$(".modal-body").html(data);
-	});
-});
-
 //For admin login 
 
 $(document).on("click", '#admin', function() {
 
 	$.get('/login', function(data) {
-//		$(".jumbotron").html(data);
 		$(".modal-body").html(data);
 	});
 });
@@ -43,6 +35,45 @@ $(document).on("click", '#admin', function() {
 $(document).on("click", '#dash_admin', function() {
 
 	$.get('/admin', function(data) {
-		$(".abc").html(data);
+		$(".scoreboard").html(data);
 	});
+});
+//For playing team list
+$(document).on("click", '#playing_team', function() {
+
+	$.get('/list', function(data) {
+		$(".scoreboard").html(data);
+	});
+});
+//For playing team list
+$(document).on("click", '#add_team', function() {
+
+	$.get('/addTeam', function(data) {
+		console.log(data);
+	$(".modal-body").html(data);
+	});
+});
+//For select playing 11 player for team 1
+$(document).on("change", '#playing_team_A_select', function() {
+	var id = $(this).val();
+	$.get('/teamPlayer/' + id, function(data) {
+		$(document).find('#player_list_A').html(data);
+	});
+});
+//For select playing 11 player for team 2
+$(document).on("change", '#playing_team_B_select', function() {
+	var first_team = $('#playing_team_A_select').val();
+	var id = $(this).val();
+	if(first_team == id) {
+		alert("This team is already selected for Today match");
+	} 
+	else {
+		$.get('/teamPlayer/' + id, function(data) {
+			$(document).find('#player_list_B').html(data);
+		});
+	}
+});
+//For numeric input only
+jQuery('.onlyNumeric').keyup(function () { 
+    this.value = this.value.replace(/[^0-9\.]/g,'');
 });
