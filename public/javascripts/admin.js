4f5757aa5e39f53ed504d317b073e5f60dc76f29
jyoti02 <jyoti.kumari@osscube.com>
2015-07-24 17:54:47 +0530

    scoreboard

--- public/javascripts/admin.js
+++ public/javascripts/admin.js
@@ -1,3 +1,8 @@
+/**
+ * @author jyotikumari
+ */
+
+
 $(document).on('click', 'input[type="button"][class="player_btn bowler"]', function() {
 	 $('input[type="button"][class="player_btn bowler green"]').removeClass('green');
 	 $(this).addClass('green');
@@ -13,6 +18,7 @@ $(document).on('click', 'input[type="button"][class*="player_btn batsman"]', fun
 		if(count<2){
 			$("#"+batId).addClass('green');
 			count++;
+			alert("Only 2 batsman can be selected");
 		}
 	}
 	 
@@ -24,13 +30,34 @@ $(document).on('click', 'input[type="button"][class*="ball"]', function() {
 	
 	  var check_ball = $(this).attr('checkclass');	 
 	  var checkedRun=[];
+	  var check_over;
 	  $("input:checkbox[class*='overball'][checkclass="+check_ball+"]:checked ,input:radio[class*='overball'][checkclass="+check_ball+"]:checked").each(function(){
 		  checkedRun.push($(this).val());
-		});
+		 });
 	  if ( $.inArray('9', checkedRun)!=-1  || $.inArray('10', checkedRun)!=-1) {
-		 // console.log($("input:checkbox[class*='overball'][checkclass="+check_ball+"]"));
 		    $("input:checkbox[class*='overball'][checkclass="+check_ball+"]").prop('checked',false);
 		    $("input:radio[class*='overball'][checkclass="+check_ball+"]").prop('checked',false);
+		  var currentTr= $("input:checkbox[class*='overball'][checkclass="+check_ball+"]").parents('tr').attr('id');
+		  var newrow=$("<tr class="+currentTr+"><td></td>");
+		  for(var i=1; i <=10; i++) { 
+			  	if(i<=7){ 
+		  newrow.append("<td><input type='radio'  name='runScored1' class='overball ball1' checkclass='1' value= "+i+" id="+i+"></td>");
+		     } else{
+		     newrow.append("<td><input type='checkbox'  class='overball ball1' checkclass='1' value=" + i+ " id="+i+ "></td>");
+		     }
+		      } 
+		  newrow.append("<td><input type='button' value='OK' class ='ball1' checkclass='1' id='ball1'></td></tr>");
+		 
+		newrow.insertAfter($(this).parents('tr').closest( "tr" ) );
+		  /*  var table = $('table[name="score_table"]');
+		    var tr = $('tr[class=currentRow]');
+		    var tdRadio = $('<input />', {'class' : 'overball ball1', 'type': 'radio'});
+		    var tdCheckBox = $('<input />', {'class' : 'overball ball1', 'type' : 'checkbox'});
+		    var tdAction = $('<button />', {'class' : 'ball1', html:'OK'});
+
+		    newRow(table,tr,[tdRadio,tdCheckBox,tdAction]);
+		    */
+		    
 		}else if($.inArray('8', checkedRun)==0){
 			ballcount++;
 			$("input:checkbox[class*='overball']:checked").parents('tr').next('tr').find('input[type=radio]').show();
@@ -45,11 +72,13 @@ $(document).on('click', 'input[type="button"][class*="ball"]', function() {
 		}
 	  if(ballcount==6){
 		  overcount++;
-		  var check_over = $('input[type="button"][class*="over_btn"]').attr('id');
+		  check_over = $('input[type="button"][class*="over_btn"]').attr('id');
 		  $("#"+check_over).css("background-color", "red");
 		  var nextover=$('input[type="button"][class*="over_btn"]').next().attr('id');
 		  $("#"+nextover).css("background-color", "rgb(123, 123, 211)");
 		  $("input:checkbox[class*='overball']").prop('checked',false);
 		    $("input:radio[class*='overball']").prop('checked',false);
 	  }
+	 
 });
+
